b5162039b81c0310b6ef0b34bc4cae8e9ebf47d5
Persiano99 <sorenrahimi99@gmail.com>
2024-03-08 14:01:44 +0100

    immagine

---
+++ src/immagine.java
@@ -0,0 +1,24 @@
+public class immagine extends elementoMultimediale {
+    private int luminosita;
+    public immagine(String titolo, int luminosita) {
+        super(titolo, 0);
+        this.luminosita = luminosita;
+    }
+
+    public void aumentaLuminosita(){
+        luminosita++;
+    }
+
+    public void abbassaLuminosita(){
+        if(luminosita > 0){
+            luminosita--;
+        }
+    }
+
+    public void show(){
+        for (int i = 0; i < luminosita; i++){
+            System.out.println("*");
+        }
+        System.out.println(" " + titolo);
+    }
+}
